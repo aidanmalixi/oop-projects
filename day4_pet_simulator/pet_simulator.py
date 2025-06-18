8f9d7a25d4c58834b7ec037e815df95b881f35f4
aidanmalixi <aidsofficials@gmail.com>
2025-06-18 17:52:29 -0400

    More changes to pet_simulator. Need to account for edge cases (min,max)

--- day4_pet_simulator/pet_simulator.py
+++ day4_pet_simulator/pet_simulator.py
@@ -10,6 +10,7 @@ class Pet:
         self.happiness_level +=2
         print(f"{self.name} has been fed.")
         print (f"{self.name} now has a hunger level of {self.hunger_level} and a happiness level of {self.happiness_level}")
+        print(f"[pet_simulator.py] __name__ is: {__name__}")
 
     def play(self):
         self.happiness_level +=2
@@ -31,3 +32,37 @@ class Pet:
         print(f"\n{self.name}'s Current Status:")
         print(f"Happiness level: {self.happiness_level}")
         print(f"Hunger level: {self.hunger_level}")
+
+def show_menu(pet):
+    print(f"1: Feed {pet.name}.")
+    print(f"2: Play with {pet.name}.")
+    print(f"3: Let {pet.name} rest.")
+    print(f"4: Show happiness and hunger status of {pet.name}.")
+    print(f"5: Exit menu.")
+
+def main():
+    pet_name = input("Please enter name for your pet: ")
+    if pet_name:
+        pet = Pet(pet_name)
+    else:
+        print("Pet name cannot be blank. Please try again.")
+    while True:
+        show_menu(pet)
+        option = int(input("Choose an option: "))
+        if option == 1:
+            pet.feed()
+        elif option == 2:
+            pet.play()
+        elif option == 3:
+            pet.rest()
+        elif option == 4:
+            pet.status()
+        elif option == 5:
+            print(f"{pet.name} will miss you!")
+            break
+        else:
+            print("Please enter a valid number in the menu.")
+
+if __name__ == "__main__":
+    main()
+    
--- day4_pet_simulator/pet_tester.py
+++ day4_pet_simulator/pet_tester.py
@@ -7,3 +7,7 @@ pet1.rest()
 pet1.feed()
 pet1.feed()
 pet1.status()
+
+print(__name__)
+
+print(f"[run_game.py] __name__ is: {__name__}")
